155ea32eb2c7a6734239d316389d2c5c8d2e5b5c
Samesh Swongamikha <samesh3mikha@hotmail.com>
2011-08-05 12:03:22 +0545

    Integrated Cf

--- app/models/weblog.rb
+++ app/models/weblog.rb
@@ -9,49 +9,16 @@ class Weblog < ActiveRecord::Base
     def checkIfPorn(url)
       puts ('++++++++++++++++++++++++++++++ CHECKING IF PORN')
       
-        title = AlchemyApi::TextExtraction.get_title_from_url(url).title
-        title = title.downcase
-        total_title_words = title.split.size
-        badwords_hash = Hash.new
-        badwords_hash = match_badwords(title)
-        badwords_count = 0
-        badwords_percent = 0
-        badwords_hash.values.each do |value|
-          badwords_count += value
-        end
-        puts ('++++++++++++++++++++++++++++++ TITLE PARSING')
-        puts (badwords_hash)
-
-        if total_title_words > 0
-          badwords_percent = (badwords_count/total_title_words) * 100
-        end
-        if badwords_percent >= 25
-          return true
-        end
-                                    
-        content = AlchemyApi::TextExtraction.get_raw_text_from_url(url).text
-        content = content.downcase
-        total_content_words = content.split.size
-        badwords_hash = Hash.new      
-        badwords_hash = match_badwords(content)
-        badwords_count = 0
-        badwords_percent = 0
-        badwords_hash.values.each do |value|
-          badwords_count += value
-        end
-        puts ('++++++++++++++++++++++++++++++ CONTENT PARSING')
-        puts (url)
-        puts (content)
-        puts (badwords_hash)
-
-        if total_content_words > 0
-          badwords_percent = (badwords_count/total_content_words) * 100
-        end      
-        if badwords_count >= 35 or badwords_percent >= 25
-          return true
-        end
+      #FETCH CF LINE
+      line = CF::Line.info("auto-adult-content-moderation-line")
       
-        return false
+      #CREATE RUN
+      run = CF::Run.create(line, "x3cf_1", ["http://google.com","http://msn.com","http://yahoo.com"])
+      
+      # FETCH OUTPUT
+      final_output = Run.final_output("x3cf_1")
+      
+      return false
     end
   
     def match_badwords(content)
