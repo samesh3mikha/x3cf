bb69345fb27e119145c7b18f9e4596d66c438168
Samesh Swongamikha <samesh3mikha@hotmail.com>
2011-08-31 09:39:10 +0545

    Sengrid changed

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port => "587",
   :domain => "x3watch.heroku.com", # TODO : May have to change this later
   :authentication => :plain,     
-  :user_name => "X3watch", # my_sendgrid_username 
-  :password => "xxxChurch", # my_sendgrid_password     
+  :user_name => "prashiddha.joshi@sprout-technology.com", # my_sendgrid_username 
+  :password => "sprouttechnology", # my_sendgrid_password     
   :tls => true,
 }
